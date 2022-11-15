a77fdf8b73aefae1b40f72fe6ebbdb8c2fdd1033
Sohail Ahmad <sohailss2412@gmail.com>
2022-11-15 19:23:52 +0530

    rotating logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from 'react'
 import { signOut } from "firebase/auth"
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
-import chatAlong from "../img/chatAlong.svg"
+import chatAlongGrey from "../img/chatAlongGrey.svg"
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext)
 
   return (
     <div className='navbar'>
-      <img className="logo" height={60} src={chatAlong} alt="logo"></img>
+      <img className="logo" height={60} src={chatAlongGrey} alt="logo"></img>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
